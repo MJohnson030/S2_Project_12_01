594de55bcd7c1e68cfa8c0a69e37675fe76acf6c
Unknown <mjohnson030@west-mec.org>
2019-02-28 18:28:20 -0700

    Created two variable
    Created two variables inside second javascript file. The second variable contains a for loop containing multiple array items for different areas inside of the cart

---
+++ tc_cart.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Millione Johnson
+   Date: 2/27/19  
+   
+   Filename: tc_cart.js
+	
+*/
+
+var orderTotal = 0;
+var cartHTML = "<table>" + "<tr>" + "<th>Item</th>" + "<th>Description</th>" + "<th>Price</th> + <th>Qty</th>" + "<th>Total</th>" + "</tr>";
+
+for (var i = 0; i >= item ; i++) {
+    cartHTMl = "<tr>" + "<td><img src='tc_item.png' alt='" + item[i] + "' />" + "</td>" + "<td>" + itemDescription[i] + "</td>" + "<td>$" + itemPrice[i] + "</td>" +  "<td>" + itemQty[i] + "</td>";
+
+
+}
+
+
+
